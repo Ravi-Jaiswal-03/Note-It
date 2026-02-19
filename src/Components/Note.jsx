d0251383db42c4afa19d57d58477c5830da56437
Ravi jaiswal <ravijaiswal3123@gmail.com>
2026-02-19 10:22:52 +0530

    initial commit

---
+++ src/Components/Note.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Note = ({id, text, editHandler, deleteHandler}) => {
+  return (
+    <div className='note'>
+        <div className='note_body'>{text}</div>
+        <div className='note_footer' style={{justifyContent : "flex-end"}}>
+        <button className='note_save' onClick={() => deleteHandler(id)}>Delete</button> &nbsp;
+        <button className='note_save' onClick={() => editHandler(id, text)}>Edit</button>
+        </div>
+    </div>
+  )
+}
+
+ 
